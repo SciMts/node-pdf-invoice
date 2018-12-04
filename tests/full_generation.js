60b23f6014ecc135cb0994b889678186ae367900
Jonny Burger <jonathanburger11@gmail.com>
2018-12-04 14:48:48 +0100

    update test to adapt to scimts

--- tests/full_generation.js
+++ tests/full_generation.js
@@ -1,31 +1,29 @@
-const fs = require('fs')
-const path = require('path')
-const generator = require('../dist/index')
+const fs = require('fs');
+const path = require('path');
+const generator = require('../dist/index');
 
 const document = generator({
-  company: {
-    phone: '(99) 9 9999-9999',
-    email: 'company@evilcorp.com',
-    address: 'Av. Companhia, 182, Água Branca, Piauí',
-    name: 'Evil Corp.',
-  },
-  customer: {
-    name: 'Elliot Raque',
-    email: 'raque@gmail.com',
-  },
-  items: [
-    {amount: 50.0, name: 'XYZ', description: 'Lorem ipsum dollor sit amet', quantity: 12},
-    {amount: 12.0, name: 'ABC', description: 'Lorem ipsum dollor sit amet', quantity: 12},
-    {amount: 127.72, name: 'DFE', description: 'Lorem ipsum dollor sit amet', quantity: 12},
-  ],
-})
+	company: {
+		email: 'info@sciencematters.io',
+		address: 'Bahnhofstrasse 3\n8003 Zürich',
+		name: 'Sciencematters AG'
+	},
+	customer: {
+		name: 'Elliot Raque',
+		email: 'raque@gmail.com'
+	},
+	items: [
+		{
+			amount: 600.0,
+			name: 'Article processing fee',
+			description: 'Manuscript number XXXXXXXXX',
+			quantity: 1
+		}
+	]
+});
 
-document.generate()
+document.generate();
 
-document
-  .pdfkitDoc
-  .pipe(
-    fs.createWriteStream(
-      path.join(process.cwd(), 'tests/testing.pdf')
-    )
-  )
+document.pdfkitDoc.pipe(
+	fs.createWriteStream(path.join(process.cwd(), 'tests/testing.pdf'))
+);
